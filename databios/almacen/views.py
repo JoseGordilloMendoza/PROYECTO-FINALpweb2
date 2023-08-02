a6795417b93edd6e7759fd41af85e7b9cb5932c7
Alejandro Phocco <aphoccot@unsa.edu.pe>
2023-08-02 17:50:32 -0500

    Se creo el api para hacer las consultas de los productos

--- databios/almacen/views.py
+++ databios/almacen/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Product,Categoria
+from rest_framework import generics
+from .serializer import ProductSerializer
+
 def homeView(request):
     return render(request, 'index.html')
 
@@ -13,5 +16,14 @@ def catalogo(request):
     }
     return render(request, 'catalogo.html', context)
 
-def filtroCategoria(request):
-    return render(request, 'categorias.html')
+class ProductAPI(generics.ListAPIView):
+    serializer_class = ProductSerializer
+    
+    def get_queryset(self):
+        categorias_especificas = self.request.query_params.getlist('categorias')
+        queryset = Product.objects.all()
+
+        for categoria in categorias_especificas:
+            queryset = queryset.filter(categorias__nombre=categoria)
+
+        return queryset
--- databios/databios/urls.py
+++ databios/databios/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from almacen.views import homeView,catalogo
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('almacen.urls'), name='api'),
     path('admin/', admin.site.urls),
     path('', catalogo, name='Homeview'),
     path('catalogo/', catalogo, name='productos'),

ad5a0609c584b9dc8e479e425004e75789a37d60
Alejandro Phocco <aphoccot@unsa.edu.pe>
2023-08-03 13:41:28 -0500

    Se agrego el .gitignore

--- databios/almacen/static/javascript/filter.js
+++ databios/almacen/static/javascript/filter.js
@@ -11,26 +11,24 @@ function obtenerSelect(selectElement) {
      return response.json()}).then(data => {console.log(data);
      toHtml(data)});
 }
-
+hj
 function toHtml(data) {
   texto = ''
 
   data.forEach(element => {
     texto += `
-          <div class="product">
-              <h1 class="product-title">${element.nombre} </h1>
-              
-              <div class="img-container">
-                  <img src='${element.imagen}' alt = '${element.nombre} 'class="product-image">
-              </div>
-  
-              <h2 class="product-description">${element.descripcion} </h2>
-              <h2>Stock: ${element.stock} </h2>
-              <h1 class="product-price">Precio: S/${element.precio} </h1>
-              <button class="buy-button">COMPRAR</button>
-          </div>
+    <div class="col-lg-4 col-md-6 col-sm-6">
+      <div class="product__item">
+        <div class="product__item__pic set-bg">
+          <img src="${element.imagen}" alt="${element.nombre}">
+        </div>
+        <div class="product__item__text">
+          <h6><a href="#">${element.nombre}</a></h6>
+          <h5>$${element.precio}</h5>
+        </div>
+      </div>
+    </div>
     `
-    
   });
   div = document.querySelector('.content');
   div.innerHTML = texto;

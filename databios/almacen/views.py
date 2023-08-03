35c6c73137f65011d68c33f46ec85ac8fe3e2354
Jose Gordillo <you@example.comjgordillome@unsa.edu.oe>
2023-08-03 18:32:51 -0500

    Funcionalidad de usuarios implementada

--- databios/almacen/views.py
+++ databios/almacen/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from .models import Product,Categoria
+from django.urls import reverse_lazy
+
 
 def homeView(request):
   return render(request, 'index.html')
@@ -13,3 +16,51 @@ def catalogo(request):
     'categorias': categorias,
     }
   return render(request, 'catalogo.html', context)
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ['categorias', 'nombre', 'descripcion', 'precio', 'imagen', 'stock']
+    template_name = 'product_form.html'  # Nombre de la plantilla para el formulario de creación de producto
+    success_url = '/'  # URL de redirección después de guardar un nuevo producto
+
+    def form_valid(self, form):
+        # Guarda el objeto del formulario antes de añadir la relación muchos a muchos
+        product = form.save()
+
+        # Obtiene las categorías seleccionadas en el formulario
+        categorias = form.cleaned_data['categorias']
+        
+        # Añade las categorías seleccionadas al modelo Product
+        product.categorias.set(categorias)
+        
+        return super().form_valid(form)
+    
+def product_list(request):
+    # Obtener todos los productos
+    products = Product.objects.all()
+    return render(request, 'product_list.html', {'products': products})
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ['categorias', 'nombre', 'descripcion', 'precio', 'imagen', 'stock']
+    template_name = 'product_edit.html'  # Nombre de la plantilla para el formulario de edición de producto
+    success_url = '/'  # URL de redirección después de guardar los cambios en el producto
+
+    def form_valid(self, form):
+        # Realiza acciones adicionales antes de guardar los cambios en el producto (opcional)
+        return super().form_valid(form)
+    
+
+def product_delete_list(request):
+    # Obtener todos los productos
+    products = Product.objects.all()
+    return render(request, 'product_delete_list.html', {'products': products})
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'product_confirm_delete.html'  # Nombre de la plantilla para la confirmación de eliminación
+    success_url = reverse_lazy('product_list')  # URL de redirección después de eliminar un producto
+
+    def form_valid(self, form):
+        # Realiza acciones adicionales antes de eliminar el producto (opcional)
+        return super().form_valid(form)
--- databios/databios/urls.py
+++ databios/databios/urls.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
 from django.urls import path
-from almacen.views import homeView,catalogo
+from almacen.views import homeView,catalogo, ProductDeleteView,ProductCreateView,ProductUpdateView, product_list, product_delete_list
 from django.conf import settings
 from django.conf.urls.static import static
-from account.views import register,login_view, logout_view
+from account.views import register, loginView, logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homeView, name='Homeview'),
     path('catalogo/', catalogo, name='productos'),
     path('register/', register, name='registro'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+    path('login/', loginView, name='login'),
+    path('logout/', logout, name='logout'),
+    path('product/create/', ProductCreateView.as_view(), name='create'),
+    path('product/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_edit'),
+    path('product/list/', product_list, name='product_list'),
+    path('product/delete/list/', product_delete_list, name='product_delete_list'),
+    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
+
+
+
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   

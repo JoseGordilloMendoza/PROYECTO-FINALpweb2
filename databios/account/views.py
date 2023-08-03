d774d9ac006b1e0e2e1bf7db892f9467141c1e4f
Jose Gordillo <you@example.comjgordillome@unsa.edu.oe>
2023-08-02 22:59:27 -0500

    vista de registro para probar

--- databios/account/views.py
+++ databios/account/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from almacen.views import catalogo
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(catalogo)  # Redirige a la página deseada después de un registro exitoso
+    else:
+        form = RegistrationForm()
+    
+    return render(request, 'register.html', {'form': form})
+

f979b95ec771d92fcd95bac907ba73dea8227da6
Jose Gordillo <you@example.comjgordillome@unsa.edu.oe>
2023-08-02 22:58:12 -0500

    modelo de usuario personalizado

--- databios/account/models.py
+++ databios/account/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
+from almacen.models import ShoppingCar
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField()
+    telefono_celular = models.CharField(max_length=20, blank=True, null=True, validators=[MinLengthValidator(9)])
+    carrito = models.OneToOneField(ShoppingCar, null=True, on_delete=models.CASCADE)
+    # Reemplaza 'nombre_de_tu_app' con el nombre de tu aplicación donde está definido el modelo ShoppingCar
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=('groups'),
+        blank=True,
+        related_name='custom_users'  # Aquí defines el related_name para el modelo Group
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=('user permissions'),
+        blank=True,
+        related_name='custom_users_permissions'  # Aquí defines el related_name para el modelo Permission
+    )
+    def __str__(self):
+       return self.username

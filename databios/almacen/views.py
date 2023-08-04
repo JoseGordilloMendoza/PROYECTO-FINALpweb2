90d2844f6781bd503266a6c6e7b712196ef5d48e
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2023-08-03 22:09:09 -0500

    plantilla general por trabajar, funcionalidad pendiente

--- databios/almacen/views.py
+++ databios/almacen/views.py
@@ -7,7 +7,6 @@ from django.urls import reverse_lazy
 def homeView(request):
   return render(request, 'index.html')
 
-
 def catalogo(request):
   obj = Product.objects.all()
   categorias = Categoria.objects.all()
@@ -16,6 +15,30 @@ def catalogo(request):
     'categorias': categorias,
     }
   return render(request, 'catalogo.html', context)
+
+def inicioView(request):
+    return render(request, 'inicio.html')
+
+def filtroproductosView(request):
+    return render(request, 'filtroproductos.html')
+
+def detalletiendaView(request):
+    return render(request, 'detalletienda.html')
+
+def carritoView(request):
+    return render(request, 'carrito.html')
+
+def detallesBlogView(request):
+    return render(request, 'detallesBlog.html')
+
+def verificacionView(request):
+    return render(request, 'verificacion.html')
+
+def blogView(request):
+    return render(request, 'blog.html')
+
+def contactoView(request):
+    return render(request, 'contacto.html')
 
 class ProductCreateView(CreateView):
     model = Product
--- databios/databios/urls.py
+++ databios/databios/urls.py
@@ -4,6 +4,7 @@ from almacen.views import homeView,catalogo, ProductDeleteView,ProductCreateView
 from django.conf import settings
 from django.conf.urls.static import static
 from account.views import register, loginView, logout
+from almacen.views  import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,7 +18,15 @@ urlpatterns = [
     path('product/list/', product_list, name='product_list'),
     path('product/delete/list/', product_delete_list, name='product_delete_list'),
     path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
-
+    path('catalogo/', catalogo, name='productos'),
+    path('inicio/', inicioView, name='inicio'),
+    path('filtroproductos/', filtroproductosView, name='filtroproductos'),
+    path('detalletienda/', detalletiendaView, name='detalletienda'),
+    path('carrito/', carritoView, name='carrito'),
+    path('detallesBlog/', detallesBlogView, name='detallesBlog'),
+    path('verificacion/', verificacionView, name='verificacion'),
+    path('blog/', blogView, name='blog'),
+    path('contacto/', contactoView, name='contacto'),
 
 
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   

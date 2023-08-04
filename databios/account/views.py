37bec72f6f4d43d592104ffbe0fb90fb71ab9f48
Jose Gordillo <you@example.comjgordillome@unsa.edu.oe>
2023-08-04 08:03:57 -0500

    Email de verificacion por registro

--- databios/account/views.py
+++ databios/account/views.py
@@ -3,6 +3,16 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
 from almacen.models import ShoppingCar, itemCar, Product
+from django.core.mail import send_mail
+from django.conf import settings
+
+def send_verification_email(email):
+    subject = 'Verificación de correo'
+    message = 'Gracias por registrarte. Tu cuenta fue creada con exito'
+    from_email = settings.EMAIL_HOST_USER
+    recipient_list = [email]
+
+    send_mail(subject, message, from_email, recipient_list)
 
 def loginView(request):
     if request.method == 'POST':
@@ -43,6 +53,7 @@ def register(request):
                 user = User.objects.create_user(username=username, password=password1, email=email,first_name=first_name,last_name=last_name)
                 user.save();
                 print('user created')
+                send_verification_email(email)
                 return redirect('/login')
 
         else:
